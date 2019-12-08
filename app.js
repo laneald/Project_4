77e57f99bc70aecbef48985c4db0a85d69fd17eb
Laneal Dickerson <laneald@icloud.com>
2019-12-08 15:15:58 -0500

    Changes to sign-in, sign-up forms and app.js

--- app.js
+++ app.js
@@ -3,16 +3,34 @@ const bodyParser = require('body-parser');
 const request = require('request');
 
 const app = express();
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(express.static('public'));
 
-app.get('/signup', function (req, res) {
-    res.sendFile(__dirname + '/signup.html');
+app.get('/sign-up', function (req, res) {
+    res.sendFile(__dirname + '/sign-up.html');
 });
 
-app.get('/signin', function (req, res) {
-    res.sendFile(__dirname + '/signin.html');
+app.post('/sign-up', function (req, res) {
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    console.log(firstName, lastName, email, password);
+});
+
+app.get('/sign-in', function (req, res) {
+    res.sendFile(__dirname + '/sign-in.html');
+});
+
+app.post('/sign-in', function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    console.log(email, password);
 });
 
 app.get('/index', function (req, res) {

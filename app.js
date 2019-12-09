4449bd37c8e6c91e2325f00c0c7a03144e5f9f57
Laneal Dickerson <laneald@icloud.com>
2019-12-09 14:21:50 -0500

    Added files and other major changes

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+//jshint esversion:6
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
+
 const app = express();
+
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 app.use(express.static('public'));
+
+var isLoggedIn = false;
+var name = '';
 
 app.get('/sign-up', function (req, res) {
     res.sendFile(__dirname + '/sign-up.html');
@@ -30,11 +38,45 @@ app.post('/sign-in', function (req, res) {
     var email = req.body.email;
     var password = req.body.password;
 
-    console.log(email, password);
+    isLoggedIn = true;
+    name = email.substring(0, email.lastIndexOf('@'));
+
+    console.log(name, email, password, isLoggedIn);
+
+    res.redirect('/index');
 });
 
 app.get('/index', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    //res.sendFile(__dirname + '/index.html');
+
+    res.render('index', {
+        authPass: isLoggedIn,
+        fName: name
+    });
+});
+
+app.post('/inventory1', (req, res) => {
+    res.sendFile(__dirname + '/inventory1.html');
+});
+
+app.get('/inventory1', (req, res) => {
+    res.sendFile(__dirname + '/inventory1.html');
+});
+
+app.post('/inventory2', (req, res) => {
+    res.sendFile(__dirname + '/inventory2.html');
+});
+
+app.get('/inventory2', (req, res) => {
+    res.sendFile(__dirname + '/inventory2.html');
+});
+
+app.post('/inventory3', (req, res) => {
+    res.sendFile(__dirname + '/inventory3.html');
+});
+
+app.get('/inventory3', (req, res) => {
+    res.sendFile(__dirname + '/inventory3.html');
 });
 
 app.listen(3000, function () {
